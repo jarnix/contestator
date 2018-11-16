ac871fcff3c4c64bd9d10fd88e41225623ad7fde
Julien Ricard <jarnix@jarnix.com>
2018-11-16 15:59:42 +0000

    wip

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/jarnix/contestator
+
+require (
+	github.com/cenkalti/backoff v2.0.0+incompatible // indirect
+	github.com/dghubble/go-twitter v0.0.0-20181013181545-e75339c4bc3d
+	github.com/dghubble/sling v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/google/go-querystring v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20181114220301-adae6a3d119a // indirect
+)
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	_ "database/sql"
+
+	_ "github.com/dghubble/go-twitter/twitter"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// aller chercher 4 tweets via search avec #concours, rt, retweet, follow (...)
+// voir dans le tweet les comptes à suivre, s'il y a des indications particulières
+// stocker les comptes followés dans la base pour les défollower 1 mois après
+// attendre au moins 6 heures entre chaque action, les faire en journée uniquement (à heure régulière)
+
+// anti anti bot
+// poster des trucs en allant chercher les news ailleurs pour faire genre le compte est normal
+// poster des status à la con
+// retweeter des célébrités du milieu
